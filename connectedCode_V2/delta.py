a7a0a98ce4821a3bf80a0033095804de09035beb
LeokipStudent <78823909+leokip@users.noreply.github.com>
2026-09-21 15:58:07 +0200

    Calculate centrum of face for delta meassurment

--- connectedCode_V2/delta.py
+++ connectedCode_V2/delta.py
@@ -7,8 +7,9 @@ class delta():
         print("Middle_width = ", self.middle_width, "Middle_Height = ", self.middle_height)
 
     def get_delta(self, face):
-        x = face[0]
-        y = face[1]
+        x = face[0] + ( face[2] // 2 ) # Center of face
+        y = face[1] + ( face[3] // 2 ) # Center of face
+        
         delta = (int(self.middle_width - x), int(self.middle_height - y))
 
         return delta
