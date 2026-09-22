553a5f8f058570481e5403e0e994ef0d6c444d52
Paulius Kuzmarskis <likutis2009@gmail.com>
2026-09-22 15:17:02 +0200

    dfsdf

--- Servo/main.py
+++ Servo/main.py
@@ -7,9 +7,9 @@ servo = Servo(25)
 try:
     # Your servo control code goes here
     servo.min()
-    sleep(1)
+    sleep(1000)
     servo.max()
-    sleep(1)
+    sleep(1000)
     
 finally:
     # This executes no matter what, freeing the pin
