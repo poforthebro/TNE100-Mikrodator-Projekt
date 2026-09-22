8c865bdc986babf2e21223688bf7f768bcdaedd6
Paulius Kuzmarskis <likutis2009@gmail.com>
2026-09-22 14:56:29 +0200

    woof spaces

--- Servo/main.py
+++ Servo/main.py
@@ -4,12 +4,12 @@ from time import sleep
 servo = Servo(25)
 
 try:
-	while True:
-    	servo.min()
-    	sleep(0.5)
-    	servo.mid()
-    	sleep(0.5)
-    	servo.max()
-    	sleep(0.5)
+ while True:
+    servo.min()
+    sleep(0.5)
+    servo.mid()
+    sleep(0.5)
+    servo.max()
+    sleep(0.5)
 except KeyboardInterrupt:
 	print("Program stopped")
